77d610f12fd8f6d339b794fc6183c3f7ce5502bd
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-11-14 20:51:29 +0530

    first commit

---
+++ DIVIDE.py
@@ -0,0 +1,4 @@
+print("Give number to divide.")
+num1 = int(input("Enter your number1:"))
+num2 = int(input("Enter your number1:"))
+print("The answer is",num1 / num2)
---
+++ SLICING AND LENGTH.py
@@ -0,0 +1,5 @@
+quote = "Past is a waste paper, present is a newspaper and future is a question paper"
+print(quote)
+print("This is quote has",len(quote), "characters")
+print(quote[5])
+print(quote[23:30])
